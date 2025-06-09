5ce40d60b04b25068d374a3539fced3d295504c8
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-09 15:58:55 +0530

    while loop

---
+++ whileExample.java
@@ -0,0 +1,22 @@
+public class whileExample {
+    public static void main(String[] args) {
+        int i = 150;
+        // while(i <= 200)
+        // {
+        //     if( i % 7 == 0)
+        //     {
+        //         System.out.println(i);
+        //     }
+        // }
+        do
+        {
+            if(i % 7 == 0)
+            {
+                System.out.println(i);
+            }
+            i++;
+        }while( i <= 200);
+    }
+    
+}
+
