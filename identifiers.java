9ae40e308e62701fb34bde150c4f66d3da0d745a
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-09 14:43:15 +0530

    identifiers example

---
+++ identifiers.java
@@ -0,0 +1,16 @@
+class identifiers
+{
+    static String SareeName = "Banarasi";
+    static double price = 2000.00f;
+    static String fabricType= "Cotton";
+    
+    static void sareeDetails()
+    {
+        System.out.println(" " + SareeName);
+        System.out.println(" " + price);
+        System.out.println(" " + fabricType);
+    }
+    public static void main(String[] args) {
+        sareeDetails();
+    }
+}
