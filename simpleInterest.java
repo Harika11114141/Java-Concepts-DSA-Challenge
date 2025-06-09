cba27b332600ac3831c6efe7798a88e16aedb406
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-09 15:42:05 +0530

    calculate simple interest

---
+++ simpleInterest.java
@@ -0,0 +1,10 @@
+public class simpleInterest {
+    public static void main(String[] args) {
+        int principle = 100;
+        int time = 2;
+        float rate = 3.25f;
+
+        double total = (principle * time * rate) / 100;
+        System.out.println("totail interest " + total);
+    }
+}
