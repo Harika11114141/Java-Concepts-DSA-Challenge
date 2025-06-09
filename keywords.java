761e8fbb36b68ad24dcf568ec4f4a5438527d4ea
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-09 14:52:23 +0530

    java keywords

---
+++ keywords.java
@@ -0,0 +1,13 @@
+public class keywords {
+    public static void main(String[] args) {
+        int sareePrice = 2000;
+        if (sareePrice > 1000) 
+        {
+            System.out.println("Expensive");
+        }
+        else
+        {
+            System.out.println("Affordable");
+        }
+    }
+}
