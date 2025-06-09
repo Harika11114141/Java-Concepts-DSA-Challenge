efad35f04ffa4fa7290bc253e4270d1d117d4e86
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-09 14:47:45 +0530

    instance variables

---
+++ InstanceVar.java
@@ -0,0 +1,15 @@
+public class InstanceVar {
+    int number = 10;
+    void see()
+    {
+        System.out.println("see this side " + number);
+    }
+    public static void main(String[] args) {
+        InstanceVar obj1 = new InstanceVar();
+        obj1.see();
+
+        InstanceVar obj2 = new InstanceVar();
+        obj2.number=100;
+        obj2.see();
+    }
+}
