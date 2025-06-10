ec686fdb4f18cae3722f1f900b46b0d68dc066a5
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-10 23:51:21 +0530

    add example 2 list interface

---
+++ Collection Frameworks/ListInterfaceEx2.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+public class ListInterfaceEx2 {
+    public static void main(String[] args) {
+        List<String> movies = new ArrayList<>();
+        // add the elements
+        movies.add("Seetha ramam");
+        movies.add("Zebra");
+        movies.add("manu");
+        movies.add("Anaganaga");
+
+        System.out.println(movies);
+        
+        // for(String Final : movies)
+        // {
+        //     System.out.println("list the movie : " + Final);
+        // }
+
+        // update the elements
+        movies.set(2,"Oyy");
+        System.out.println(movies);
+        System.out.println(movies.contains("Oyy"));
+
+        // seraching a movie name
+        int result = movies.indexOf("Oyy");
+        System.out.println(result);
+
+        // remove the movie name
+        movies.remove(1);
+        System.out.println(movies);
+
+        // accessing elements
+        String j = movies.get(0);
+        System.out.println(j);
+
+        // the movie name is present or not will chesk using the contains operation
+
+        System.out.println(movies.contains("oyy"));
+
+    }
+}
