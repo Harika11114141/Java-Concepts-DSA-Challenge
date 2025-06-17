eaa76c2b2c6c4749d7aa80bee7a15483fad31c43
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-17 19:26:06 +0530

    example for diffrence between pq and pbq

---
+++ Collection Frameworks/priorityqueueEx2.java
@@ -0,0 +1,28 @@
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class priorityqueueEx2 {
+    public static void main(String[] args) throws InterruptedException{
+        Queue<String> rooms = new PriorityQueue<>();
+        PriorityBlockingQueue<Integer> floor = new PriorityBlockingQueue<>();
+        rooms.offer("3BHK");
+        rooms.add("2BHK");
+        rooms.add("1BHK");
+        rooms.add("2BHK");
+        // System.out.println(rooms);
+        // rooms.add(null); // not allowed exception
+        System.out.println(rooms);
+        // floor.offer(2);
+        // floor.add(4);
+        // floor.add(3);
+        floor.add(2);
+        Integer v = floor.take();
+        System.out.println(v);
+        // floor.add(null);
+        // System.out.println(floor); // exception
+        floor.add(3);
+        System.out.println(floor);
+    }
+}
