02a4d450fa5262f0ca138927fd22598a6e71621b
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-06-17 21:49:49 +0530

    using deque linkedlist example

---
+++ Collection Frameworks/linkedlist.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class linkedlist {
+    public static void main(String[] args) {
+        Deque<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.addFirst(88);
+        q.addLast(33);
+        System.out.println(q);
+        q.peek();
+        q.peekFirst();
+        q.offerFirst(22);
+        q.removeFirst();
+        System.out.println(q);
+    }
+}
