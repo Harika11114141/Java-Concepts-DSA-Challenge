1443bdf641092c3c041d4e1d5cc76dc1356cda36
Vuppu Venkata Harika <harikakumar2587@gmail.com>
2025-07-02 23:24:09 +0530

    sample one

--- Recursion/print_five_names.java
+++ Recursion/print_five_names.java
@@ -1,1 +1,17 @@
-"// Recursion example" 
+public class print_five_names
+{
+    static int count = 0;
+    public static void names()
+    {
+        if(count == 5)
+        {
+            return;
+        }
+        System.out.println("HArika");
+        count++;
+        names();
+    }
+    public static void main(String[] args) {
+        names();
+    }
+}
